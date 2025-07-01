cbd87a68843f51ce85a32bb929cd0eb9ac1ba95a
0hruv <dhruv1328gupta@gmail.com>
2025-07-01 15:07:05 +0000

    added mechanism

--- train.js
+++ train.js
@@ -1,15 +1,65 @@
 
-createGrid();
+start();
 
-function createGrid()
+function start(){
+    let startButton = document.querySelector(".start");
+    startButton.addEventListener("click",()=>{
+       startButton.remove();
+       gameStart();
+    })
+}
+
+
+function gameStart(){
+
+let grid = document.querySelector(".grid");
+
+let time = 15;
+let hits = 0;
+
+startTimer();
+playGame();
+
+function startTimer(){
+
+    let timer = document.querySelector(".time");
+    
+    let second = 15;
+        let countdown = setInterval(() =>{
+            second--;
+            time = second;
+            timer.textContent = `TIME: ${second}`;
+
+            if(second == 0)
+            {
+                clearInterval(countdown);
+                document.querySelector(".target").remove();
+            }
+        },1000)
+
+}
+
+
+
+function playGame()
 {
-    let grid = document.querySelector(".grid");
-
     let target = document.createElement("div");
     target.className = "target";
 
     grid.appendChild(target);
+    changePosition(target);
 
+    target.addEventListener("click",() =>{
+        hits++;
+        let hitCount = document.querySelector(".score")
+        hitCount.textContent = `HITS : ${hits}`;
+        changePosition(target);
+    });
+
+}
+
+function changePosition(target)
+{
     let gridHeight = target.parentElement.offsetHeight;
     let gridWidth = target.parentElement.offsetWidth;
 
@@ -20,6 +70,5 @@ function createGrid()
 
     target.style.top = targetHeight;
     target.style.right = targetWidth;
-
-    
+}
 }
